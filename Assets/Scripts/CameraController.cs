b5ec3e5a70d21b04e4f2a5e302652333315f54c3
Luis Borjas <luisfborjas@gmail.com>
2015-11-26 02:05:25 -0600

    All changes until lesson 7
    http://unity3d.com/learn/tutorials/projects/roll-a-ball/displaying-text?playlist=17141

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// LateUpdate is called once per frame, but guaranteed to run after all items
+	// have been rendered; so it happens after the player actually moves
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
